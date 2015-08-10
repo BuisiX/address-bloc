a8883dbd3781d69799804c98f2fcd39f311c1503
BuisiX <nwabuisi33@gmail.com>
2015-08-10 16:52:19 -0500

    Delete entries

--- models/address_book.rb
+++ models/address_book.rb
@@ -19,4 +19,16 @@ class AddressBook
 
 		@entries.insert(index, Entry.new(name, phone, email))
 	end
+
+	def remove_entry(name, phone, email)
+		index = 0
+		@entries.each do |entry|
+			if name < entry.name
+				break
+			end
+			index += 1
+		end
+
+		@entries.delete(index)
+	end
 end

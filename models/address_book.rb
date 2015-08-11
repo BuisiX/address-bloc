ad90b73d268248593b0e994030f00d6b61e24caa
BuisiX <nwabuisi33@gmail.com>
2015-08-10 21:17:37 -0500

    Created address menu

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,22 +1,23 @@
-require_relative "entry.rb"
+ require_relative "entry.rb"
+ 
+ class AddressBook
+   attr_accessor :entries
 
-class AddressBook
-	attr_accessor :entries
+   def initialize
+     @entries = []
+   end
+   
+   def add_entry(name, phone, email)
+ 
+     index = 0
+     @entries.each do |entry|
 
-	def initialize
-		@entries = []
-	end
+       if name < entry.name
+         break
+       end
+       index += 1
+     end
 
-	def add_entry(name, phone, email)
-		index = 0
-		@entries.each do |entry|
-
-			if name < entry.name
-				break
-			end
-			index += 1
-		end
-
-		@entries.insert(index, Entry.new(name, phone, email))
-	end
-end
+     @entries.insert(index, Entry.new(name, phone, email))
+   end
+ end
